8d0cb935059f3a72ecbb6c57191519a893ffd0e2
Kyle Terry <kyle@kyleterry.com>
2011-12-20 12:58:53 -0600

    Added view template

--- books/views.py
+++ books/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Count
+
 from annoying.decorators import render_to
 
 from books.models import Book
@@ -5,8 +7,9 @@ from books.models import Book
 
 @render_to('books/list.html')
 def list(request):
-    return {'books': Book.objects.filter(user=request.user)}
+    return {'books': Book.objects.filter(user=request.user).annotate(num_authors=Count('authors')).order_by('num_authors')}
 
 
+@render_to('books/view.html')
 def view(request, book_id):
     return {'book': Book.objects.get(pk=book_id)}
